189c4f908c1ddfd92eaf2cc9a1880c41cd599c75
Jon Hawks <conall.hawks@gmail.com>
2016-10-12 22:05:46 -0400

    Performance & reliability enhancements.

--- misc/global.js
+++ misc/global.js
@@ -1,60 +1,72 @@
+/* ######################################################################### */
+/* #### Custom polyfills. ################################################## */
+/* ######################################################################### */
+
+// Element.matches()
+Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector||Element.prototype.oMatchesSelector||Element.prototype.webkitMatchesSelector||function(e){for(var t=(this.document||this.ownerDocument).querySelectorAll(e),o=t.length;--o>=0&&t.item(o)!==this;);return o>-1});
+
+/* ########################################################################## */
+/* #### Throttle. ###################### Usage: throttle(function, time) #### */
+/* ########################################################################## */
+function throttle(n,l,t){var a,e,u,r,i=0;t||(t={});var o=function(){i=t.leading===!1?0:Date.now(),a=null,r=n.apply(e,u),a||(e=u=null)},c=function(){var c=Date.now();i||t.leading!==!1||(i=c);var f=l-(c-i);return e=this,u=arguments,0>=f||f>l?(a&&(clearTimeout(a),a=null),i=c,r=n.apply(e,u),a||(e=u=null)):a||t.trailing===!1||(a=setTimeout(o,f)),r};return c.cancel=function(){clearTimeout(a),i=0,a=e=u=null},c};
+
 /* ########################################################################## */
 /* #### Fancy Page Loading ################################################## */
 /* ########################################################################## */
 
 // Throttle prevents lag.
-$(window).scroll($.throttle(100, contentSlider));
+$(window).scroll(throttle(contentSlider, 100));
 $(window).resize(contentSlider);
 function contentSlider(){
-	if(typeof contentSlider.window === 'undefined') contentSlider.window = $(window);
-	var top = contentSlider.window.scrollTop();
-	var height = contentSlider.window.height();
-	var fraction = top + height * .9;
-	var element = $('.content-box:not(:first-of-type)');//possible filter(): if already in viewport don't select? would remove dependency in ajax callback
-	element.each(function(index){
-		if($(element[index]).offset().top > fraction){
-			$(element[index]).css('transition', '.5s all, .25s opacity');
-			$(element[index]).css('left', (index % 2 ? '150%' : '-150%'));
-			$(element[index]).css('opacity', 0);
-		}else{
-			$(element[index]).css('transition', '.8s all, 1.15s opacity');
-			$(element[index]).css('left', '0%');
-			$(element[index]).css('opacity', 1);
-		}
-	});
+    if(typeof contentSlider.window === 'undefined') contentSlider.window = $(window);
+    var top = contentSlider.window.scrollTop();
+    var height = contentSlider.window.height();
+    var fraction = top + height * .9;
+    var element = $('.content-box:not(:first-of-type)');//possible filter(): if already in viewport don't select? would remove dependency in ajax callback
+    element.each(function(index){
+        if($(element[index]).offset().top > fraction){
+            $(element[index]).css('transition', '.5s all, .25s opacity');
+            $(element[index]).css('left', (index % 2 ? '150%' : '-150%'));
+            $(element[index]).css('opacity', 0);
+        }else{
+            $(element[index]).css('transition', '.8s all, 1.15s opacity');
+            $(element[index]).css('left', '0%');
+            $(element[index]).css('opacity', 1);
+        }
+    });
 }
 
 // Header box cells slide-in.
 $(document).ready(function(){
-	var element = $('.header-box td');
-	var delay = 450;
-	var time = 1000;
-	element.css('left', '100%');
-	element.css('transition', time + 'ms');
-	element.each(function(index){
-		setTimeout(function(){
-			$(element[index]).css('left', '0%');
-		}, delay);
-		delay += 50;
-	});
-	setTimeout(function(){element.removeAttr('style')}, time + delay);
+    var element = $('.header-box td');
+    var delay = 450;
+    var time = 1000;
+    element.css('left', '100%');
+    element.css('transition', time + 'ms');
+    element.each(function(index){
+        setTimeout(function(){
+            $(element[index]).css('left', '0%');
+        }, delay);
+        delay += 50;
+    });
+    setTimeout(function(){element.removeAttr('style')}, time + delay);
 });
 
 function asideSlideOut(){
-	var element = $('.content-nav, .left-box');
-	element.css('left', '-100%');
-	element.css('opacity', 0);
+    var element = $('.content-nav, .left-box');
+    element.css('left', '-100%');
+    element.css('opacity', 0);
 }
 
 function contentSlideOut(){
-	var element = $('.content-box');
-	element.css('transition', '.5s all, .25s opacity');
-	setTimeout(function(){
-		element.each(function(index){
-				$(element[index]).css('left', (index % 2 ? '-150%' : '150%'));
-				$(element[index]).css('opacity', 0);
-		});
-	}, 1);
+    var element = $('.content-box');
+    element.css('transition', '.5s all, .25s opacity');
+    setTimeout(function(){
+        element.each(function(index){
+                $(element[index]).css('left', (index % 2 ? '-150%' : '150%'));
+                $(element[index]).css('opacity', 0);
+        });
+    }, 1);
 }
 
 /* ########################################################################## */
@@ -62,21 +74,21 @@ function contentSlideOut(){
 /* ########################################################################## */
 
 // Throttle prevents lag.
-$(window).scroll($.throttle(100, navbar));
+$(window).scroll(throttle(navbar, 100));
 
 // Only show the navbar if we need it.
 function navbar(){
-	if(typeof navbar.element === 'undefined'){
-		navbar.element = $('.navbar');
-		navbar.headerHeight = $('.header').height();
-	}
-	var viewOffset = $(window).scrollTop();
-	
-	if(viewOffset > navbar.headerHeight){
-		navbar.element.addClass('navbar-show');
-	}else if(navbar.element.hasClass('navbar-show')){
-		navbar.element.removeClass('navbar-show');
-	}
+    if(typeof navbar.element === 'undefined'){
+        navbar.element = $('.navbar');
+        navbar.headerHeight = $('.header').height();
+    }
+    var viewOffset = $(window).scrollTop();
+
+    if(viewOffset > navbar.headerHeight){
+        navbar.element.addClass('navbar-show');
+    }else if(navbar.element.hasClass('navbar-show')){
+        navbar.element.removeClass('navbar-show');
+    }
 }
 
 /* ########################################################################## */
@@ -85,30 +97,31 @@ function navbar(){
 
 // Change the Content when an AJAX link is clicked.
 $(document).on('click', '.ajax-link', function(event){
-	event.preventDefault();
-	if(window.location.pathname !== this.pathname) setState(this.href);
+    event.preventDefault();
+    if(window.location.pathname !== this.pathname) setState(this.href);
 });
 
 // Synchronize with a new state.
 function setState(link){
-	if(typeof setState.header === 'undefined') setState.header = $('.header-h2');
-	if(typeof setState.content === 'undefined') setState.content = $('.content');
-	asideSlideOut();
-	contentSlideOut();
-	document.title = 'Jon Hawks \u2622 Loading...';
-	setState.header.text('Loading...');
-	setState.header.css({'opacity': .25, 'transition': '.5s opacity'});
-	setTimeout(function(){
-		setState.content.load(link, function(){
-			contentSlider();
-			document.title = 'Jon Hawks';
-			setState.header.text('/' + link.split('/')[link.split('/').length - 1].replace('.html', ''));
-			setState.header.css('opacity', 1);
-			
-			// Code highlighting.
-			$('code').each(function(i, block){hljs.highlightBlock(block);});
-		});
-	}, 400);
+    if(typeof setState.header === 'undefined') setState.header = $('.header-h2');
+    if(typeof setState.content === 'undefined') setState.content = $('.content');
+    asideSlideOut();
+    contentSlideOut();
+    document.title = 'Jon Hawks \u2622 Loading...';
+    setState.header.text('Loading...');
+    setState.header.css({'opacity': .25, 'transition': '.5s opacity'});
+    setTimeout(function(){
+        setState.content.load(link, function(){
+            contentSlider();
+            document.title = 'Jon Hawks';
+            setState.header.text('/' + link.split('/')[link.split('/').length - 1].replace('.html', ''));
+            setState.header.css('opacity', 1);
+
+            // Code highlighting.
+            $("code").each(function(i, block){hljs.highlightBlock(block)});
+
+        });
+    }, 400);
 }
 
 /* ########################################################################## */
@@ -116,63 +129,63 @@ function setState(link){
 /* ########################################################################## */
 $(document).ready(function(){$('.calendar').text(calendar())});
 function calendar(){
-	calendar.date = new Date();
-	calendar.year = calendar.date.getFullYear();
-	calendar.month = calendar.date.getMonth();
-	calendar.day = calendar.date.getDate();
-	
-	// Verbose month.
-	switch(calendar.month){
-		case 0: calendar.month = 'January'; break;
-		case 1: calendar.month = 'February'; break;
-		case 2: calendar.month = 'March'; break;
-		case 3: calendar.month = 'April'; break;
-		case 4: calendar.month = 'May'; break;
-		case 5: calendar.month = 'June'; break;
-		case 6: calendar.month = 'July'; break;
-		case 7: calendar.month = 'August'; break;
-		case 8: calendar.month = 'September'; break;
-		case 9: calendar.month = 'October'; break;
-		case 10: calendar.month = 'November'; break;
-		case 11: calendar.month = 'December'; break;
-	}
-	
-	// Date suffix.
-	switch(calendar.day){
-		case 1: calendar.day += 'st'; break;
-		case 2: calendar.day += 'nd'; break;
-		case 3: calendar.day += 'rd'; break;
-		case 21: calendar.day += 'st'; break;
-		case 22: calendar.day += 'nd'; break;
-		case 23: calendar.day += 'rd'; break;
-		case 31: calendar.day += 'st'; break;
-		default: calendar.day += 'th'; break;
-	}
-	
-	return calendar.month + ' ' + calendar.day + ', ' + calendar.year;
+    calendar.date = new Date();
+    calendar.year = calendar.date.getFullYear();
+    calendar.month = calendar.date.getMonth();
+    calendar.day = calendar.date.getDate();
+
+    // Verbose month.
+    switch(calendar.month){
+        case 0: calendar.month = 'January'; break;
+        case 1: calendar.month = 'February'; break;
+        case 2: calendar.month = 'March'; break;
+        case 3: calendar.month = 'April'; break;
+        case 4: calendar.month = 'May'; break;
+        case 5: calendar.month = 'June'; break;
+        case 6: calendar.month = 'July'; break;
+        case 7: calendar.month = 'August'; break;
+        case 8: calendar.month = 'September'; break;
+        case 9: calendar.month = 'October'; break;
+        case 10: calendar.month = 'November'; break;
+        case 11: calendar.month = 'December'; break;
+    }
+
+    // Date suffix.
+    switch(calendar.day){
+        case 1: calendar.day += 'st'; break;
+        case 2: calendar.day += 'nd'; break;
+        case 3: calendar.day += 'rd'; break;
+        case 21: calendar.day += 'st'; break;
+        case 22: calendar.day += 'nd'; break;
+        case 23: calendar.day += 'rd'; break;
+        case 31: calendar.day += 'st'; break;
+        default: calendar.day += 'th'; break;
+    }
+
+    return calendar.month + ' ' + calendar.day + ', ' + calendar.year;
 }
 
 setInterval(function(){$('.clock').text(clock())}, 1000);
 function clock(){
-	clock.time = new Date();
-	clock.hours = clock.time.getHours();
-	clock.minutes = clock.time.getMinutes();
-	clock.seconds = clock.time.getSeconds();
-	
-	// Leading zeroes.
-	if(clock.minutes < 10) clock.minutes = '0' + clock.minutes;
-	if(clock.seconds < 10) clock.seconds = '0' + clock.seconds;
-	
-	// Calculate ante meridiem or post meridiem.
-	if(clock.hours > 12){
-		clock.meridiem = 'PM';
-		clock.hours -= 12;
-	}else{
-		clock.meridiem = 'AM';
-	}
-	
-	// Zero hour is midnight.
-	if(clock.hours == 0) clock.hours = 12;
-	
-	return clock.hours + ':' + clock.minutes + ':' + clock.seconds + ' ' + clock.meridiem;
+    clock.time = new Date();
+    clock.hours = clock.time.getHours();
+    clock.minutes = clock.time.getMinutes();
+    clock.seconds = clock.time.getSeconds();
+
+    // Leading zeroes.
+    if(clock.minutes < 10) clock.minutes = '0' + clock.minutes;
+    if(clock.seconds < 10) clock.seconds = '0' + clock.seconds;
+
+    // Calculate ante meridiem or post meridiem.
+    if(clock.hours > 12){
+        clock.meridiem = 'PM';
+        clock.hours -= 12;
+    }else{
+        clock.meridiem = 'AM';
+    }
+
+    // Zero hour is midnight.
+    if(clock.hours == 0) clock.hours = 12;
+
+    return clock.hours + ':' + clock.minutes + ':' + clock.seconds + ' ' + clock.meridiem;
 }
